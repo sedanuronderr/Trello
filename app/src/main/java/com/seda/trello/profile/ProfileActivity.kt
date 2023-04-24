7246d10c99152d2379df7218b72c7b1c107dd897
Seda Nur Önder <sednrondr@gmail.com>
2023-04-24 17:26:39 +0300

    image update

--- app/src/main/java/com/seda/trello/profile/ProfileActivity.kt
+++ app/src/main/java/com/seda/trello/profile/ProfileActivity.kt
@@ -1,22 +1,28 @@
 package com.seda.trello.profile
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.provider.MediaStore
 import android.widget.Toast
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 import com.bumptech.glide.Glide
+import com.seda.trello.Manifest
 import com.seda.trello.ObjectClass
 import com.seda.trello.R
 import com.seda.trello.databinding.ActivityProfileBinding
 import com.seda.trello.fragments.MainActivity2
 import com.seda.trello.model.User
+import com.seda.trello.utils.Constants
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
-    private var doubleBackToExitPressedOnce = false
+    private var mSelectedImageFileUri : Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,10 +44,51 @@ class ProfileActivity : AppCompatActivity() {
 
 
         binding.navImage1.setOnClickListener{
-            if()
+            if(ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+                      showImageChooser()
+
+            }else{
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.READ_STORAGE_PERMISSION_CODE
+                )
+            }
         }
 
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty()&& grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    showImageChooser()
+            }
+        }else{
+            Toast.makeText(this,"Opps,you just denied the permission for storage.You can also allow it from settings",Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+
+    private fun showImageChooser(){
+        var galleryIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+         startActivityForResult(galleryIntent,Constants.PICK_IMAGE_REQUEST_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == Constants.PICK_IMAGE_REQUEST_CODE && data!!.data != null){
+            mSelectedImageFileUri = data.data
+        }
+
+    }
+
 
     fun setUserData(dataUser: User?) {
         Glide
